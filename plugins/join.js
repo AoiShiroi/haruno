aeed47f71788034f4e6f1919a6684aafb957818a
AoiShiroi <91419617+AoiShiroi@users.noreply.github.com>
2021-10-03 21:48:47 +0700

    Add files via upload

--- plugins/join.js
+++ plugins/join.js
@@ -2,11 +2,12 @@ let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 
 let handler = async (m, { conn, text, usedPrefix, args }) => {
     let user = db.data.users[m.sender]
-    if (joincount === 3 ) throw 'Anda sudah melebihi batas maksimal memasukkan bot sebagai user premium.'
+    if (user.joincount === 3 ) throw 'Anda sudah melebihi batas maksimal memasukkan bot sebagai user premium.'
     user.joincount += 1
     m.reply(`Joincount mu: ${user.joincount}/3`)
     if (!args[0]) throw 'Masukkan link group'
     if (!args[1]) throw 'Masukkan jumlah hari'
+    if (args[1] > 30) throw `Wee.. jan banyak banyak lah`
     let [_, code] = args[0].match(linkRegex) || []
     if (!code) throw 'Link Salah'
     let res = await conn.acceptInvite(code)
