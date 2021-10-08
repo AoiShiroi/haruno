dd6af7d31a6d64400cafd9b6adc80070f3a5d334
AoiShiroi <91419617+AoiShiroi@users.noreply.github.com>
2021-10-08 18:59:40 +0700

    Add files via upload

--- plugins/event.js
+++ plugins/event.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 let handler = async(m, { conn, text}) => {
     if (!text) {
         conn.send2ButtonLoc(m.chat, await(await fetch(image)).buffer(), `Event belum di buat, silahkan request ke owner.`, watermark, 'Menu', '.?', 'Request ke Qodir', '.event qodir')
-    } else if (text === 'qodir') this.sendContact(m.chat, '6283153189868@s.whatsapp.net', 'Qodir', m)
+    } else if (text === 'qodir') conn.sendContact(m.chat, '6283153189868@s.whatsapp.net', 'Qodir', m)
 }
 handler.command = /^event$/i
 handler.tags = ['main']
